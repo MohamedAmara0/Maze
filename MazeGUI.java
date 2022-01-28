859dd6eae1801d5d6c260e79d6fdd789a9867b25
MohamedAmara0 <93670207+MohamedAmara0@users.noreply.github.com>
2022-01-28 01:23:07 -0500

    Create MazeGUI.java

---
+++ MazeGUI.java
@@ -0,0 +1,12 @@
+package code;
+
+/**
+* @author Mohamed Amara + Raed Ahmed + Olivia Jasinskas + Miles Carter
+* Runs the Maze game.
+*/
+
+public class MazeGUI {
+	public static void main(String[] args){
+		new MainMenu();
+	}
+}
